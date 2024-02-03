0c7095673ecc1cd406a2bdbf570937a303b6333a
Longgererer <2072451919@qq.com>
2024-02-03 16:52:44 +0800

    feat: 迁移样式

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,6 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import App from "./App.vue"
 
-// themes
-import "../public/css/font.css"
-
 const pinia = createPinia()
 const app = createApp(App)
 
