058de407c6ef03d6b817da0ba353cbd2b5c9f80d
Longgererer <2072451919@qq.com>
2024-03-20 16:50:12 +0800

    feat: 删除无用代码

--- src/utils/config/save.ts
+++ src/utils/config/save.ts
@@ -1,6 +1,2 @@
-import { IEditorLibraries, IEditorPrepMap } from "@type/settings"
-import { TemplateLang, TemplateType } from "@type/template"
-import { IndexedDBStore } from "@utils/services/indexed-db-service"
-
 /** 存在storage的key */
 export const STORAGE_NAME = "JS-Encoder-Store"
